969a8ec0bf8eee72bada4b1254d58c4ddbccb691
Deren B <dbozerprogramming@gmail.com>
2022-07-30 20:34:14 -0400

    Create the Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <h1>Hello World Round 3</h1>
+        </>
+    );
+}
